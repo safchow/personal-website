541cfa87b1b8f7a6e6e548f17100d0e3872cf466
Safwaan Chowdhury <safwaan.chowdhury@relayfi.com>
2026-03-04 15:55:55 -0500

    add links to other pages

--- frontend/src/common/PageLayout/components/Footer.tsx
+++ frontend/src/common/PageLayout/components/Footer.tsx
@@ -3,18 +3,22 @@ import * as React from 'react';
 
 import { Typography } from '@/common/Typography';
 
+const LINKEDIN_URL = 'https://www.linkedin.com/in/safwaan-hchowdhury/';
+const GITHUB_URL = 'https://github.com/safchow';
+
 export const Footer: React.FC = () => {
   return (
     <div className="w-full h-fit flex flex-row justify-between">
       <div className="flex flex-row gap-6">
         <Button variant="ghost">
-          <Typography className="text-base font-light">Email</Typography>
+          <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+            <Typography className="text-base font-light">LinkedIn</Typography>
+          </a>
         </Button>
-        <Button variant="ghost">
-          <Typography className="text-base font-light">LinkedIn</Typography>
-        </Button>
-        <Button variant="ghost">
-          <Typography className="text-base font-light">GitHub</Typography>
+        <Button asChild variant="ghost">
+          <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
+            <Typography className="text-base font-light">GitHub</Typography>
+          </a>
         </Button>
       </div>
 
