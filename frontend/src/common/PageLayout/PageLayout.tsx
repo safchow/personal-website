76481650f750aa8c6ed9eafb79deb65ebee8122e
Safewaan <safwaanchowdhury26@gmail.com>
2026-03-05 06:06:31 -0500

    fade in animation

--- frontend/src/common/PageLayout/PageLayout.tsx
+++ frontend/src/common/PageLayout/PageLayout.tsx
@@ -9,9 +9,15 @@ interface PageLayoutProps {
 export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col justify-between min-h-screen px-20 py-8">
-      <Header />
-      {children}
-      <Footer />
+      <div className="animate-fade-in [animation-delay:0ms]">
+        <Header />
+      </div>
+      <div className="animate-fade-in opacity-0 [animation-delay:100ms]">
+        {children}
+      </div>
+      <div className="animate-fade-in opacity-0 [animation-delay:200ms]">
+        <Footer />
+      </div>
     </div>
   );
 };
